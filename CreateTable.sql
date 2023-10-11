6c3880b24707692ddc845810ee70cacaa56e7eeb
Andres Castro <andrescn20@gmail.com>
2023-10-11 01:00:24 -0600

    Add queries to repo

---
+++ CreateTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Tasks] (
+    [ID]           INT           IDENTITY (1, 1) NOT NULL,
+    [title]        VARCHAR (60)  NOT NULL,
+    [description]  VARCHAR (250) NOT NULL,
+    [registeredAt] DATETIME      NOT NULL,
+    [isCompleted]  BIT           DEFAULT ((0)) NOT NULL,
+    [completedAt]  DATETIME      DEFAULT (NULL) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
