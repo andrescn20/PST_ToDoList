4ba18542ab53acacdfac460c5bc5c789daeff2ed
Andres Castro <andrescn20@gmail.com>
2023-10-11 00:40:46 -0600

    Styles and Alerts

--- ToDoList/wwwroot/js/Pages/Task/ListTask.js
+++ ToDoList/wwwroot/js/Pages/Task/ListTask.js
@@ -1,17 +1,42 @@
 ﻿$(".deleteBtn").click(function () {
     let taskId = $(this).data('task-id');
+    console.log(taskId)
+
+    $.confirm({
+        title: 'Atención: está a punto de borrar una Tarea.',
+        content: 'Seleccione "Confirmar" para continuar:',
+        buttons: {
+            confirmar: function () {
+                deleteTask(taskId)
+                $.confirm({
+                    title: 'Operación Exitosa',
+                    content: `Tarea #${taskId} eliminada con éxito`,
+                    buttons: {
+                        confirmar: function () {
+                            location.reload();
+                        },
+                    }
+                });
+            },
+            cancelar: function () {
+                $.alert('Cancelado. La tarea no fue borrada');
+            },
+        }
+    });
+})
+
+function deleteTask(taskId) {
     $.ajax({
         url: '/Task/Delete/' + taskId,
-        type: 'POST', 
+        type: 'POST',
         success: function (result) {
-            alert(`Tarea #${taskId} eliminada con éxito`)
-            location.reload();
         },
         error: function (xhr, status, error) {
             console.log(error)
         }
     });
-});
+
+}
 
 $(".editBtn").click(function () {
     let taskId = $(this).data('task-id');
